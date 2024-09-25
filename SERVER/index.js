b02dd4981bbf9650acee13350c7c5ef5f262f970
Bikash <nayakbiku18a@gmail.com>
2024-09-25 17:12:35 +0530

    change

--- SERVER/index.js
+++ SERVER/index.js
@@ -4,12 +4,15 @@ const cors=require('cors')
 const app=express();
 require('dotenv').config()
 const dbConnection=require('./models/db')
-const PORT=process.env.PORT||8080;
+const PORT=process.env.PORT ;
 
 app.use(cors());
 app.use('/',router)
 
 dbConnection();
+app.get('/',(req,res)=>{
+    res.send("Hello")
+})
 app.listen(PORT,()=>{
     console.log(`Server is listening on port ${PORT} `)
 })
