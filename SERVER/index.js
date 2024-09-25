ff22d5f6b709862fc334193baa95dceaab30c5bd
Bikash <nayakbiku18a@gmail.com>
2024-09-25 18:38:37 +0530

    change

--- SERVER/index.js
+++ SERVER/index.js
@@ -7,6 +7,12 @@ const dbConnection=require('./models/db')
 const PORT=process.env.PORT ;
 
 app.use(cors());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://create-link-tau.vercel.app"); // Adjust this accordingly
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
 app.use('/',router)
 
 dbConnection();
