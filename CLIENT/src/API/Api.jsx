d9282bf133d882fb54356277256942ad179cc26a
Bikash <nayakbiku18a@gmail.com>
2024-09-25 18:13:58 +0530

    editing files

--- CLIENT/src/API/Api.jsx
+++ CLIENT/src/API/Api.jsx
@@ -3,6 +3,7 @@ const URL=`https://create-link-server.vercel.app/`
 export const uploadFile=async(data)=>{
     try {
         const response=await axios.post(`${URL}upload`,data)
+        // console.log(response.data)
         return response.data;
     } catch (error) {
         console.error(`Error while calling the api`,error.message)
--- SERVER/utils/upload.js
+++ SERVER/utils/upload.js
@@ -1,17 +1,17 @@
 const multer=require('multer');
-const path = require('path');
+// const path = require('path');
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, '/tmp/uploads'); // Use /tmp instead of /var/task/uploads
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  }
-});
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, '/tmp/uploads'); // Use /tmp instead of /var/task/uploads
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, file.originalname);
+//   }
+// });
 
-const upload = multer({ storage: storage });
-// const upload=multer({dest:'uploads'})
+// const upload = multer({ storage: storage });
+const upload=multer({dest:'uploads'})
 
 
 module.exports=upload
