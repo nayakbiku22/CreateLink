8c75aed01112be2af5e62b2be96cd605cfe10863
Bikash <nayakbiku18a@gmail.com>
2024-09-25 16:51:49 +0530

    changing in upload.js

--- SERVER/utils/upload.js
+++ SERVER/utils/upload.js
@@ -1,6 +1,17 @@
 const multer=require('multer');
+const path = require('path');
 
-const upload=multer({dest:'uploads'})
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '/tmp/uploads'); // Use /tmp instead of /var/task/uploads
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+
+const upload = multer({ storage: storage });
+// const upload=multer({dest:'uploads'})
 
 
 module.exports=upload
